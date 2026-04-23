f40cbbd6b3616fb6a6bee1004f5e2f20916d27a6
Bekzhan <bekzhanizbasar228@gmail.com>
2026-04-23 13:38:46 +0500

    Initial Commit

---
+++ src/test/java/io/bekezhan/alertsgrafana/AlertsGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bekezhan.alertsgrafana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertsGrafanaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
